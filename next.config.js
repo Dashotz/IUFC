a9a802841cda3091b61d483311670974b9769af6
Dashotz <d.shotz@yahoo.com>
2026-01-13 01:02:39 +0800

    v1.7.15
    fixing video asset

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/media/[name].[hash][ext]',
+      },
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
